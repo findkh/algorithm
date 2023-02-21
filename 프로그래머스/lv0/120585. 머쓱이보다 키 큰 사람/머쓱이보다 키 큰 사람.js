fa8f7d3cca3956a97487966979f5e71c95889614
KyoungHyunPark <89373222+findkh@users.noreply.github.com>
2023-02-21 18:58:17 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,9 @@
 function solution(array, height) {
-        let cnt = 0;
-        array.forEach(function(i) {
-            if(i > height) {
-                cnt++;
-            }
-        })
-        return cnt;
+    let cnt = 0;
+    array.forEach(function(i) {
+        if(i > height) {
+            cnt++;
+        }
+    })
+    return cnt;
 }
