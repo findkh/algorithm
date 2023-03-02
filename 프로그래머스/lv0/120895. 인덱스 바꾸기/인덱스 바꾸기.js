f9b034cac2e331a130cca55fadc833523ee40897
KyoungHyunPark <89373222+findkh@users.noreply.github.com>
2023-03-02 19:10:50 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,9 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    
+    let strArr = [... my_string];
+    strArr.splice(num1, 1, my_string[num2]);
+    strArr.splice(num2, 1, my_string[num1]);
+    
+    return strArr.join('');
+}
