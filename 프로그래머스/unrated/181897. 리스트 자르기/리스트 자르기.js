eeacf5048c69c2952e11c94f08d2ad4a6330855b
KyoungHyunPark <89373222+findkh@users.noreply.github.com>
2023-07-04 19:22:22 +0900

    [unrated] Title: 리스트 자르기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,16 @@
+function solution(n, slicer, num_list) {
+    let answer;
+
+    if (n == 1) {
+        answer = num_list.slice(0, slicer[1] + 1);
+    } else if (n == 2) {
+        answer = num_list.slice(slicer[0], num_list.length);
+    } else if (n == 3) {
+        answer = num_list.slice(slicer[0], slicer[1] + 1);
+    } else {
+        answer = num_list.slice(slicer[0], slicer[1] + 1);
+        answer = answer.filter((_, index) => index % slicer[2] == 0);
+    }
+    
+    return answer;
+}
