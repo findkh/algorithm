759ef38c37139836d9cbd9bc94377b4de82f1235
KyoungHyunPark <89373222+findkh@users.noreply.github.com>
2023-10-26 20:28:41 +0900

    [level unrated] Title: x 사이의 개수, Time: 2.81 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.js
@@ -0,0 +1,17 @@
+function solution(myString) {
+    let result = [];
+    let currentLength = 0;
+
+    for (let i = 0; i < myString.length; i++) {
+        if (myString[i] === 'x') {
+            result.push(currentLength);
+            currentLength = 0;
+        } else {
+            currentLength++;
+        }
+    }
+
+    result.push(currentLength);
+
+    return result;
+}
